f1cf0c194450b7dc9917d708eb61ae2a31f0b77c
Dominique Quatravaux <dominique@quatravaux.org>
2018-04-16 19:48:20 +0200

    Split out GML.pm

---
+++ GML.pm
@@ -0,0 +1,61 @@
+package GML;
+
+use strict;
+use v5.26;
+
+use base qw(Exporter); our @EXPORT = qw(decode_gml);
+
+use Parse::RecDescent;
+sub decode_gml {
+  my ($gml_txt) = @_;
+
+  if ($ENV{DEBUG}) {
+    $::RD_ERRORS = 1;       # Report fatal errors
+    $::RD_WARN   = 1;       # Also report non-fatal problems
+    $::RD_HINT   = 1;       # Also suggest remedies
+    $::RD_TRACE  = 1;       # Trace the parsing nitty-gritties
+  }
+
+  state $parser = do {
+    Parse::RecDescent->new(q(
+      # See http://www.fim.uni-passau.de/fileadmin/files/lehrstuhl/brandenburg/projekte/gml/gml-technical-report.pdf
+
+      graph :         'graph'  '['      kvs    thing_in_graph(s?) ']' {
+         my ( undef,  undef,   undef,  $kvs,  $things) = @item;
+         $return = {
+           %$kvs,
+           edges    => [grep { $_->isa("GML::Edge") }   @$things],
+           vertices => [grep { $_->isa("GML::Vertex") } @$things]
+         };
+      }
+
+      thing_in_graph : node | edge
+
+      node : "node" '[' kvs ']' { $return = GML::Vertex->new(%{$item[-2]}); }
+      edge : "edge" '[' kvs ']' { $return = GML::Edge->new(%{$item[-2]}); }
+
+      kv : key value { my (undef, $k, $v) = @item; $return = [$k => $v] }
+      kvs : kv(s?) { my (undef, $kvs_ref) = @item; $return = {map {@$_} @$kvs_ref}  }
+
+      key: "comment" | "directed" | "IsPlanar" | "id" | "label" | "source" | "target"
+
+      value : integer | quoted_string
+
+      quoted_string : <perl_quotelike>  { $return = $item[1]->[2] }
+
+      integer : /[-+]?\d+/
+     ))
+  };
+
+  return $parser->graph($gml_txt);
+}
+
+sub GML::Edge::new {
+  my $class = shift;
+  return bless { @_ }, $class;
+}
+
+sub GML::Vertex::new {
+  my $class = shift;
+  return bless { @_ }, $class;
+}
--- prepare-news.pl
+++ prepare-news.pl
@@ -6,9 +6,6 @@ use warnings;
 use strict;
 use autodie;
 
-use FindBin qw($Dir); use lib $Dir;
-use STISRV13;
-
 use YAML;
 use JSON;
 use IO::All;
@@ -17,63 +14,17 @@ use Text::CSV_XS qw(csv);
 use FindBin qw($Dir);
 use String::Similarity qw(similarity);
 
+use FindBin qw($Dir); use lib $Dir;
+use STISRV13;
+use GML;
+
 my $newsatone_meta = decode_json(scalar io('newsatone-meta.json')->slurp);
-my $site_graph = decode_gml(scalar io('sti-website.gml')->slurp);
-use Data::Dumper; say Dumper($site_graph);
+my $site_graph = decode_gml(scalar io('sti-website.gml')->slurp);  # Takes ~10 secs
+my $secrets = YAML::LoadFile('./secrets.yaml');
+my $schema = STISRV13->connect(-password => $secrets->{mysql_password});
+
+
+
 
 ######################################################
 
-use Parse::RecDescent;
-sub decode_gml {
-  my ($gml_txt) = @_;
-
-  if ($ENV{DEBUG}) {
-    $::RD_ERRORS = 1;       # Report fatal errors
-    $::RD_WARN   = 1;       # Also report non-fatal problems
-    $::RD_HINT   = 1;       # Also suggest remedies
-    $::RD_TRACE  = 1;       # Trace the parsing nitty-gritties
-  }
-
-  state $parser = do {
-    Parse::RecDescent->new(q(
-      # See http://www.fim.uni-passau.de/fileadmin/files/lehrstuhl/brandenburg/projekte/gml/gml-technical-report.pdf
-
-      graph :         'graph'  '['      kvs    thing_in_graph(s?) ']' {
-         my ( undef,  undef,   undef,  $kvs,  $things) = @item;
-         $return = {
-           %$kvs,
-           edges    => [grep { $_->isa("GML::Edge") }   @$things],
-           vertices => [grep { $_->isa("GML::Vertex") } @$things]
-         };
-      }
-
-      thing_in_graph : node | edge
-
-      node : "node" '[' kvs ']' { $return = GML::Vertex->new(%{$item[-2]}); }
-      edge : "edge" '[' kvs ']' { $return = GML::Edge->new(%{$item[-2]}); }
-
-      kv : key value { my (undef, $k, $v) = @item; $return = [$k => $v] }
-      kvs : kv(s?) { my (undef, $kvs_ref) = @item; $return = {map {@$_} @$kvs_ref}  }
-
-      key: "comment" | "directed" | "IsPlanar" | "id" | "label" | "source" | "target"
-
-      value : integer | quoted_string
-
-      quoted_string : <perl_quotelike>  { $return = $item[1]->[2] }
-
-      integer : /[-+]?\d+/
-     ))
-  };
-
-  return $parser->graph($gml_txt);
-}
-
-sub GML::Edge::new {
-  my $class = shift;
-  return bless { @_ }, $class;
-}
-
-sub GML::Vertex::new {
-  my $class = shift;
-  return bless { @_ }, $class;
-}
