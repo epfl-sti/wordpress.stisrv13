80583bd32239232ac6849530b8e47fd531d831bb
Dominique Quatravaux <dominique@quatravaux.org>
2018-04-17 13:17:31 +0200

    Add auto fields, language auto-detection

--- STISRV13.pm
+++ STISRV13.pm
@@ -1,9 +1,154 @@
-package STISRV13::Article;
+package STISRV13::DatabaseRow;
 
 use base qw/DBIx::Class::Core/;
 
+# http://search.cpan.org/~blblack/DBIx-Class-0.07006/lib/DBIx/Class/Manual/Cookbook.pod#Debugging_DBIx::Class_objects_with_Data::Dumper
+sub _dumper_hook {
+  $_[0] = bless {
+    %{ $_[0] },
+    result_source => undef,
+  }, ref($_[0]);
+}
+
+package STISRV13::Article;
+
+use strict;
+use base qw(STISRV13::DatabaseRow);
+
+use Debug::Statements;
+Debug::Statements::setFlag('$STISRV13::Article::d');
+our ($d) = ($ENV{DEBUG} or '') =~ m/STISRV13=(\d+)/;
+
+use String::Similarity qw(similarity);
+use Lingua::Identify qw(langof);
+
 __PACKAGE__->table('rss');
 __PACKAGE__->add_columns(qw(rss_id author headline entete eng fra pubdate cible superstructure supertarget img imglink externallink alt view domain forward));
+
+sub webmaster_author {
+  my $self = shift;
+  local $_ = $self->author;
+
+  # STI staff
+  m/Dietrich|Reinhard/              && return "dreinhar";
+  m/\wUtz\w/                        && return "dutz";
+  m/^PGB/                           && return "pgaybalm";
+  m/Gay.Balmaz/                     && return "pgaybalm";
+  m/Homeira/                        && return "sunderla";
+  m/^Ingrid/                        && return "ifischer";
+  m/^John/                          && return "maxwell";
+  m/^Isabelle/                      && return "imorel";  # Isabelle Schafer
+  m/^L\W+A\b/                       && return "pessina";
+  m/^(Laure-Anne|Anne-Laure|Lara)/i && return "pessina";
+  m/^(Pessina)/i                    && return "pessina";
+  m/^(nbo|Nicolas)/i                && return "nborboen";
+
+  # Mediacom
+  m/Barraud/       && return "ebarraud";
+  m/^Corinn?e/     && return "cfeuz";
+  m/\wRauss\w/     && return "rauss";
+  m/^Sandy\w/      && return "cmundwil";  # Santy Evangelista
+  m/^Sarah Perrin/ && return "sperrin";
+
+  # Research Office
+  m/Patricia Marti-Rochat/ && return "pamarti";
+
+  return;
+}
+
+
+sub academic_author {
+  my $self = shift;
+  local $_ = $self->author;
+
+  m|/| && return; m|,| && return;  # Co-authors not handled (yet)
+
+  m/^Aude Billard$/      && return 115671;
+  m/^Auke Ijspeert/      && return 115955;
+  m/Vandevyver/          && return 138412;
+  m/^Dario/              && return 258955;
+  m/Van De Ville/i       && return 152027;
+  m/Farhad/              && return 106170;
+  m/Bleuler/             && return 104561;
+  m/Sanctuary|^Hillary$/ && return 131093;
+  m/^Nanni$/             && return 167918;  # Giovanni de Micheli
+  m/^Nanolab/i           && return 122431;  # Mihut Ionescu
+  m/^Nik\b/i             && return 234623;  # Nik Papageorgiou
+  m/Patrick Mayor/       && return 111483;
+  m/\wpv-lab\w/i         && return 100192;  # Christophe Ballif
+  m/Samuel Zimmermann/   && return 244428;
+
+  return;
+}
+
+sub in_the_media_author {
+  my $self = shift;
+  local $_ = $self->author;
+
+  m/\w(BBC|CNN)\w/ && return $1;
+
+  return;
+}
+
+sub corp_author {
+  my $self = shift;
+  local $_ = $self->author;
+
+  m/\w(EMC)\w/       && return "EMC";
+  m/\w(Leclanché)\w/ && return "Leclanché";
+  m/Marcel Benoist/  && return "Marcel Benoist Foundation";
+
+  return;
+}
+
+sub languages {
+  my ($self) = @_;
+
+  my @langs = grep {length($self->body) > 10} (qw(en fr));
+  return @langs unless (scalar(@langs) == 2);
+
+  my $rss_id = $self->rss_id;
+  my ($body_en, $body_fr) = map { scalar $self->body($_) } qw(en fr);
+  if (similarity($body_en, $body_fr) > 0.98) {
+    my $guessed = langof($body_en);
+    if ($guessed eq 'en') {
+      return qw(en);
+    } elsif ($guessed eq 'fr') {
+      d q($rss_id appears to be written in French);
+      return qw(fr);
+    } else {
+      d q($rss_id appears to be written in a weird language);
+      d q($guessed is the language);
+      return qw(en);   # ¯\_(ツ)_/¯
+    }
+  }
+  return @langs;
+}
+
+sub body {
+  my ($self, $lang) = @_;
+  return ($lang eq "en" ? $self->eng : $self->fra);
+}
+
+sub essentials {
+  my ($self, $lang) = @_;
+  die "Bad \$lang: $lang" unless $lang =~ m/^(en|fr)$/i;
+
+  my %retval = (
+    lang                => $lang,
+    rss_id              => $self->rss_id,
+    title               => $lang eq "en" ? $self->headline : $self->entete,
+    body                => $self->body($lang),
+    academic_author     => scalar $self->academic_author,
+    webmaster_author    => scalar $self->webmaster_author,
+    in_the_media_author => scalar $self->in_the_media_author,
+    corp_author         => scalar $self->corp_author,
+   );
+  foreach my $k (keys(%retval)) {
+    delete $retval{$k} unless defined $retval{$k};
+  }
+  return \%retval;
+}
 
 package STISRV13;
 
--- prepare-news.pl
+++ prepare-news.pl
@@ -12,19 +12,54 @@ use IO::All;
 use Text::CSV_XS qw(csv);
 
 use FindBin qw($Dir);
-use String::Similarity qw(similarity);
+use Memoize;
 
 use FindBin qw($Dir); use lib $Dir;
 use STISRV13;
 use GML;
 
 my $newsatone_meta = decode_json(scalar io('newsatone-meta.json')->slurp);
-my $site_graph = decode_gml(scalar io('sti-website.gml')->slurp);  # Takes ~10 secs
+my $site_graph; # = decode_gml(scalar io('sti-website.gml')->slurp);  # Takes ~10 secs
+warn "Data loaded.";
+
 my $secrets = YAML::LoadFile('./secrets.yaml');
 my $schema = STISRV13->connect(-password => $secrets->{mysql_password});
 
+say YAML::Dump([map { $_->essentials } Article->all($schema, $newsatone_meta, $site_graph)]);
 
+##############################################
+package Article;
 
+use base qw(Class::Delegate);
 
-######################################################
+sub all {
+  my ($class, $schema, $newsatone_meta, $site_graph) = @_;
+  my @results;
+  foreach my $dbic ($schema->resultset('Article')->all) {
+    foreach my $lang ($dbic->languages) {
+      my $elem = $class->new($dbic, $lang);
+      # TODO: Weed out articles that are published in only one language
+      push @results, $elem;
+    }
+  }
+  return @results;
+}
 
+sub new {
+  my ($class, $dbic, $lang) = @_;
+  my $self = bless { dbic => $dbic, lang => $lang }, $class;
+  $self->add_delegate($dbic);
+  return $self;
+}
+
+sub essentials {
+  my $self = shift;
+  return $self->{dbic}->essentials($self->{lang})
+}
+
+sub path_to_root {
+}
+
+# DBIx::Class::_Util freaks out when DESTROY is called twice, which it would
+# (through the delegate) if we didn't do this:
+sub DESTROY {}
